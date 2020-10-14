99a44841dfb44887657a214b385b277194478552
swilcox21 <swilcox21@gmail.com>
2020-10-14 15:02:03 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,20 +5,17 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	return <div className="text-center mt-5" />;
 }
+
+// Components : home.js,  details,  character card,  planet card
+
+// run 2 fetches on HOME.JS : planets,  characters
+
+// create 2 arrays in our state :  planets,  characters
+
+// make home.js a class component
+
+// we need 2 MAP() : planets, characters
+
+// GOOOD LUCK!!
